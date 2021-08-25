9100da399436050acae2a28555b06173b7be5c74
Max Gaurav <max.gaurav@ithands.net>
2021-08-25 18:02:28 +0530

    chnage: success login redirection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, UseInterceptors } from '@nestjs/common';
 import { UrlGeneratorService } from 'nestjs-url-generator';
 import { LoginController } from './auth/controllers/login/login.controller';
+import { SessionErrorValidationInterceptor } from './session-manager/interceptors/session-error-validation/session-error-validation.interceptor';
+import { OldInputsInterceptor } from './session-manager/interceptors/old-inputs/old-inputs.interceptor';
 
+@UseInterceptors(SessionErrorValidationInterceptor, OldInputsInterceptor)
 @Controller()
 export class AppController {
   constructor(private readonly urlGenerator: UrlGeneratorService) {}
