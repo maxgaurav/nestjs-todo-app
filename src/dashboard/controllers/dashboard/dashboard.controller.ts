94a37ced809ca6de7ccf4d2516bbf5afa6afafa7
Max Gaurav <max.gaurav@ithands.net>
2021-08-26 13:14:51 +0530

    add: listing of tasks with edit action

--- src/dashboard/controllers/dashboard/dashboard.controller.ts
+++ src/dashboard/controllers/dashboard/dashboard.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get, Render, UseGuards } from '@nestjs/common';
 import { WebGuard } from '../../../auth/guards/web/web.guard';
+import { TaskRepoService } from '../../../task/services/task-repo/task-repo.service';
+import { TaskModel } from '../../../databases/models/task.model';
+import { AuthUser } from '../../../auth/decorators/auth-user.decorator';
+import { UserModel } from '../../../databases/models/user.model';
+import { ReqTransaction } from '../../../transaction-manager/decorators/transaction/transaction.decorator';
+import { Transaction } from 'sequelize';
 
+@UseGuards(WebGuard)
 @Controller('dashboard')
 export class DashboardController {
-  @UseGuards(WebGuard)
+  constructor(private taskRepo: TaskRepoService) {}
+
   @Get()
   @Render('auth/dashboard')
-  public dashboard() {
-    return {};
+  public async dashboard(
+    @AuthUser() user: UserModel,
+    @ReqTransaction() transaction?: Transaction,
+  ): Promise<{ tasks: TaskModel[] }> {
+    return {
+      tasks: await this.taskRepo.listTaskForUser(user, transaction),
+    };
   }
 }
