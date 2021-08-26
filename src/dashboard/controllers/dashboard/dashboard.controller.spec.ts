8494351cfeaa65030603970f049377a1a4446b78
Max Gaurav <max.gaurav@ithands.net>
2021-08-26 13:29:40 +0530

    add: test cases for dashboard task listing

--- src/dashboard/controllers/dashboard/dashboard.controller.spec.ts
+++ src/dashboard/controllers/dashboard/dashboard.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DashboardController } from './dashboard.controller';
+import { TaskRepoService } from '../../../task/services/task-repo/task-repo.service';
+import { TaskModel } from '../../../databases/models/task.model';
+import { UserModel } from '../../../databases/models/user.model';
 
 describe('DashboardController', () => {
   let controller: DashboardController;
+  const taskRepo: TaskRepoService = {
+    listTaskForUser: (value) => value,
+  } as any;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DashboardController],
+      providers: [
+        {
+          provide: TaskRepoService,
+          useValue: taskRepo,
+        },
+      ],
     }).compile();
 
     controller = module.get<DashboardController>(DashboardController);
@@ -14,5 +26,22 @@ describe('DashboardController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return list of tasks to render', async () => {
+    const user: UserModel = { id: 1 } as any;
+
+    const task: TaskModel = { id: 1, user_id: user.id } as any;
+    const listSpy = jest
+      .spyOn(taskRepo, 'listTaskForUser')
+      .mockReturnValue(Promise.resolve([task]));
+    const transaction = null;
+
+    expect(await controller.dashboard(user, transaction)).toEqual(
+      expect.objectContaining({
+        tasks: [task],
+      }),
+    );
+    expect(listSpy).toHaveBeenCalledWith(user, transaction);
   });
 });
